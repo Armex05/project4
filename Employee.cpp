05c6b22df2f40b9dc7140d51bd9cc14eff547c78
Ali Armaghan <armaghanali100@gmail.com>
2024-05-22 22:32:16 +0330

    Employee Class

---
+++ Employee.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Armex on 5/22/2024.
+//
+
+#include "Employee.h"
---
+++ Employee.h
@@ -0,0 +1,14 @@
+//
+// Created by Armex on 5/22/2024.
+//
+
+#ifndef PROJECT4_EMPLOYEE_H
+#define PROJECT4_EMPLOYEE_H
+
+
+class Employee {
+
+};
+
+
+#endif //PROJECT4_EMPLOYEE_H
